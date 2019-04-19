b19597cb375ec72a294d5cfae4f49fbd72516676
flavioltonon <flavioltonon@hotmail.com>
2019-04-18 21:06:07 -0300

    update main structure cube

--- cube.go
+++ cube.go
@@ -26,31 +26,9 @@ func NewCube() *cube {
 	}
 }
 
-func (c *cube) Shuffle() {
-	for i := 0; i < 20; i++ {
-		var clockwise = rand.Int31n(1) == 1
-
-		switch faces[rand.Int31n(5)] {
-		case "front":
-			c.Rotate(clockwise).Front()
-			// case "back":
-			// 	c.Rotate(clockwise).Back()
-			// case "left":
-			// 	c.Rotate(clockwise).Left()
-			// case "right":
-			// 	c.Rotate(clockwise).Right()
-			// case "up":
-			// 	c.Rotate(clockwise).Up()
-			// case "down":
-			// 	c.Rotate(clockwise).Down()
-		}
-	}
-}
-
-func (c *cube) Rotate(clockwise bool) *rotationOption {
+func (c *cube) Rotate() *rotationOption {
 	return &rotationOption{
-		cube:      c,
-		clockwise: clockwise,
+		cube: c,
 	}
 }
 
@@ -60,30 +38,36 @@ func (c *cube) Print() *printOption {
 	}
 }
 
+func (c *cube) Shuffle() {
+	for i := 0; i < 20; i++ {
+		var clockwise = rand.Int31n(1) == 1
+
+		switch faces[rand.Int31n(5)] {
+		case "front":
+			if clockwise {
+				c.Rotate().Clockwise().Front()
+			}
+			c.Rotate().CounterClockwise().Front()
+			// case "back":
+			// 	c.Rotate().Back()
+			// case "left":
+			// 	c.Rotate().Left()
+			// case "right":
+			// 	c.Rotate().Right()
+			// case "up":
+			// 	c.Rotate().Up()
+			// case "down":
+			// 	c.Rotate().Down()
+		}
+	}
+}
+
 type face struct {
 	name   string
 	colors [][]color
 }
 
 var faces = []string{"front", "back", "left", "right", "up", "down"}
-
-func (f face) print() {
-	var white = rainbow.New(rainbow.FgWhite)
-
-	white.Print(" _________________\n")
-	for i := 0; i < 6; i++ {
-		white.Print("|")
-		for j := 0; j < 3; j++ {
-			color := f.colors[i/2][j].Color()
-			color.Print("/////")
-			white.Print("|")
-		}
-		white.Print("\n")
-		if i%2 != 0 {
-			white.Print("|-----------------|\n")
-		}
-	}
-}
 
 func (f face) MainColor() color {
 	return f.colors[1][1]
@@ -121,119 +105,4 @@ func (c color) Color() *rainbow.Color {
 	default:
 		return rainbow.New(rainbow.FgBlack)
 	}
-}
-
-type rotationOption struct {
-	cube      *cube
-	clockwise bool
-}
-
-func (r *rotationOption) Front() {
-	var (
-		front = r.cube.front.colors
-		left  = r.cube.left.colors
-		right = r.cube.right.colors
-		up    = r.cube.up.colors
-		down  = r.cube.down.colors
-		tmp   = make([]color, 3)
-	)
-
-	if r.clockwise {
-		// Handle front rotation
-		for i := 0; i < 2; i++ {
-			front[0][i], front[i][2], front[2][2-i], front[2-i][0] = front[2-i][0], front[0][i], front[i][2], front[2][2-i]
-		}
-
-		// Handle sides rotation
-		for i := 0; i < 3; i++ {
-			tmp[i] = up[2][i]
-			up[2][i] = left[2-i][2]
-			left[2-i][2] = down[0][2-i]
-			down[0][2-i] = right[i][0]
-			right[i][0] = tmp[i]
-		}
-	} else {
-		// Handle front rotation
-		for i := 0; i < 2; i++ {
-			front[0][i], front[i][2], front[2][2-i], front[2-i][0] = front[i][2-i], front[2][2-i], front[2-i][0], front[0][i]
-		}
-
-		// Handle sides rotation
-		for i := 0; i < 3; i++ {
-			tmp[i] = up[2][i]
-			up[2][i] = right[i][0]
-			right[i][0] = down[0][2-i]
-			down[0][2-i] = left[2-i][2]
-			left[2-i][2] = tmp[i]
-		}
-	}
-}
-
-func (r *rotationOption) Back() {
-	var (
-		back  = r.cube.back.colors
-		left  = r.cube.left.colors
-		right = r.cube.right.colors
-		up    = r.cube.up.colors
-		down  = r.cube.down.colors
-		tmp   = make([]color, 3)
-	)
-
-	if r.clockwise {
-		// Handle back rotation
-		for i := 0; i < 2; i++ {
-			back[0][i], back[i][2], back[2][2-i], back[2-i][0] = back[2-i][0], back[0][i], back[i][2], back[2][2-i]
-		}
-
-		// Handle sides rotation
-		for i := 0; i < 3; i++ {
-			tmp[i] = up[0][2-i]
-			up[0][2-i] = left[2-i][2]
-			left[2-i][2] = down[0][2-i]
-			down[0][2-i] = right[i][0]
-			right[i][0] = tmp[i]
-		}
-	} else {
-		// Handle back rotation
-		for i := 0; i < 2; i++ {
-			back[0][i], back[i][2], back[2][2-i], back[2-i][0] = back[i][2-i], back[2][2-i], back[2-i][0], back[0][i]
-		}
-
-		// Handle sides rotation
-		for i := 0; i < 3; i++ {
-			tmp[i] = up[0][2-i]
-			up[2][i] = right[i][0]
-			right[i][0] = down[0][2-i]
-			down[0][2-i] = left[2-i][2]
-			left[2-i][2] = tmp[i]
-		}
-	}
-}
-
-type printOption struct {
-	cube *cube
-}
-
-func (p *printOption) Front() {
-	p.cube.front.print()
-}
-
-func (p *printOption) Back() {
-	p.cube.back.print()
-}
-
-func (p *printOption) Left() {
-	p.cube.left.print()
-}
-
-func (p *printOption) Right() {
-	p.cube.right.print()
-}
-
-func (p *printOption) Up() {
-	p.cube.up.print()
-}
-
-func (p *printOption) Down() {
-	p.cube.down.print()
 }
